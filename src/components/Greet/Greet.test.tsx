a73a14869bb92f6c9906ca2c230afd6e3c1a5392
Andrew Kamiji <andrew_kamiji_dias@hotmail.com>
2022-12-05 09:00:13 -0300

    Add greet components and tests

---
+++ src/components/Greet/Greet.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { Greet } from './Greet';
+
+
+test('Greet renders correnctly', () => {
+    render(<Greet />)
+    const textElement = screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+})
+
+test('Greet renders with a name', () => {
+    render (<Greet name='Vishwas'/>)
+    const textElement = screen.getByText('Hello Vishwas')
+    expect(textElement).toBeInTheDocument()
+})
